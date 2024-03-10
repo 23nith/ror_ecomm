a7ab06cfc21bba5413755aa909538d3296dafab0
“23nith” <zenithmanuel@gmail.comgit config --global user.email ZENITHMANUEL@GMAIL.com>
2024-03-11 03:49:51 +0800

    Troubleshoot docker-entrypoint and start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -8,20 +8,7 @@ if ps -p 7 > /dev/null; then
     kill -9 7
 fi
 
-echo "testing.."
-
-# Ensure the server.pid file does not exist to prevent conflicts
-# if [ -f tmp/pids/server.pid ]; then
-#     echo "Removing existing server.pid file"
-#     rm tmp/pids/server.pid
-# fi
-
-# Attempt to start the Rails server
-# if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
-
 echo "executing rails server"
 exec ./bin/rails server
-
-
 
 echo "Rails server started successfully."
